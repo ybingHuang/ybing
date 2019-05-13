65101fb25426e955855048f85a23aa8363b74a5d
NikoHuang <>
2019-05-13 14:25:46 +0800

    添加配置信息

--- authentication/src/main/java/com/ybing/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/ybing/authentication/AuthenticationApplication.java
@@ -2,8 +2,11 @@ package com.ybing.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
