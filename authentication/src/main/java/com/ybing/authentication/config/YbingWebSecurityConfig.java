333c54c064183334b9e3478c2e94e5b7e8e47112
NikoHuang <>
2019-05-23 17:20:11 +0800

    remove exclusion import

--- authentication/src/main/java/com/ybing/authentication/config/YbingWebSecurityConfig.java
+++ authentication/src/main/java/com/ybing/authentication/config/YbingWebSecurityConfig.java
@@ -1,7 +1,5 @@
 package com.ybing.authentication.config;
 
-import com.ybing.authentication.oauth.YbingAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,16 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 /**
  * Created by niko on 2019/5/17.
  */
 @Configuration
 public class YbingWebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private YbingAuthenticationProvider ybingAuthenticationProvider;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,7 +23,6 @@ public class YbingWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        //web.ignoring().antMatchers("/resources/**");
         super.configure(web);
     }
 
